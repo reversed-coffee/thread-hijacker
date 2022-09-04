cc8401d39ac862e56babc94f9f0f305aebc403a4
Nicholas <nick@rethinkify.net>
2022-09-04 07:48:14 -0700

    Update StreamExtensions.cs

--- StreamExtensions.cs
+++ StreamExtensions.cs
@@ -9,14 +9,10 @@ namespace Injector
 {
     internal static class StreamExtensions
     {
-        public static void WritePointer(this MemoryStream stm, uint pointer)
-        {
+        public static void WritePointer(this MemoryStream stm, uint pointer) =>
             stm.Write(BitConverter.GetBytes(pointer), 0, sizeof(uint));
-        }
 
-        public static void Skip(this MemoryStream stm, int n = 1)
-        {
+        public static void Skip(this MemoryStream stm, int n = 1) =>
             stm.Position += n;
-        }
     }
 }
